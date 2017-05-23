d7e321c052b9784aa040b2bbd17c1b9922152d47
Alexei Andrushievich <vint21h@vint21h.pp.ua>
2017-05-23 15:41:32 +0300

    fix license in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import (
 
 
 # metadata
-VERSION = (0, 2, 0)
+VERSION = (0, 2, 1)
 __version__ = ".".join(map(str, VERSION))
 
 setup(
@@ -22,14 +22,14 @@ setup(
     author="Alexei Andrushievich",
     author_email="vint21h@vint21h.pp.ua",
     description="Show django messages using bootstrap-notify",
-    license="GPLv3 or later",
+    license="MIT",
     url="https://github.com/DCOD-OpenSource/django-messages-to-bootstrap-notify/",
     download_url="https://github.com/DCOD-OpenSource/django-messages-to-bootstrap-notify/archive/{version}.tar.gz".format(version=__version__),
     zip_safe=False,
     include_package_data=True,
     classifiers=[
         "Environment :: Plugins",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: Unix",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
